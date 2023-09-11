6bb26c1c3f58daec469249a07ed23615ed7544fb
iamkaikai <iamkaikai@livemail.tw>
2023-09-11 10:17:22 -0400

    fixing style bugs

--- public/style.js
+++ public/style.js
@@ -1,12 +1,18 @@
-// vanilla js
 document.addEventListener("DOMContentLoaded", function() {
     let nav = document.getElementById('top-sub-nav');
     window.addEventListener('scroll', () => {
+        
         let y = window.scrollY;
-        if (y > 44) {
-            nav.style.marginTop = '-44px';
-        } else {
+        console.log(y)
+        if (y <= 16){
             nav.style.marginTop = '0px';
+            nav.classList.remove('scroll');
+            // nav.style.top = 'unset';
+
+        }else{
+            nav.style.marginTop = '0px';
+            nav.classList.add('scroll');
+            // nav.style.top = '0px';
         }
     });
   });
