fb6e361c4b0ef067c50c17dcd29ee0baabe5bc80
iamkaikai <iamkaikai@livemail.tw>
2023-11-02 01:20:43 -0400

    bug fixed for nav

--- public/style.js
+++ public/style.js
@@ -7,12 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
         if (y <= 16){
             nav.style.marginTop = '0px';
             nav.classList.remove('scroll');
-            // nav.style.top = 'unset';
+            nav.style.top = '44px';
 
         }else{
             nav.style.marginTop = '0px';
             nav.classList.add('scroll');
-            // nav.style.top = '0px';
+            nav.style.top = '0px';
         }
     });
   });
